63274a7de0130d52b3b22f612b6a8dd544d3d02a
saraahmad1114 <asara1114@gmail.com>
2018-03-09 19:07:57 -0500

    changes

--- CreateAGraph/CreateAGraph/APIClient.swift
+++ CreateAGraph/CreateAGraph/APIClient.swift
@@ -12,6 +12,10 @@ class APIClient{
     
     class func getJSONInformation (completion: @escaping([String: Any])-> ()){
         
+        var jsonDictionary = [String: Any]()
+        
+        let url = "https://api.darksky.net/forecast/\(Secrets.apiKey)/40.7380,-73.8801"
+        
         
         
         
---
+++ CreateAGraph/CreateAGraph/Secrets.swift
@@ -0,0 +1,14 @@
+//
+//  Secrets.swift
+//  CreateAGraph
+//
+//  Created by Sara Ahmad on 3/9/18.
+//  Copyright © 2018 Sara Ahmad. All rights reserved.
+//
+
+import Foundation
+
+struct Secrets {
+    
+    static var apiKey = "e73f5a6ee5fa277ced4af79f4686747d"
+}
