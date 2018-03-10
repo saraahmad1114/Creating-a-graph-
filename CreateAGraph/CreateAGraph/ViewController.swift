37899d31eb756b0add2280c6ebd75e77591f2828
saraahmad1114 <asara1114@gmail.com>
2018-03-09 20:32:13 -0500

    storyboard stuff

--- CreateAGraph/CreateAGraph/ViewController.swift
+++ CreateAGraph/CreateAGraph/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let store = HourlyTemperatureDatastore.sharedInstance
 
     override func viewDidLoad() {
         super.viewDidLoad()
