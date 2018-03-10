5112e2430605094cd9256e7d0e6132a0fb463459
saraahmad1114 <asara1114@gmail.com>
2018-03-09 19:21:15 -0500

    changes

--- CreateAGraph/CreateAGraph/ViewController.swift
+++ CreateAGraph/CreateAGraph/ViewController.swift
@@ -12,18 +12,11 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
-        APIClient.getJSONInformation { (jsonDictionary) in
-            print("*******************************")
-            print(jsonDictionary)
-            print("*******************************")
-        }
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
