74ab83705f13add2dbc686aa356328557dcb0f39
saraahmad1114 <asara1114@gmail.com>
2018-03-10 11:17:46 -0500

    GraphView class

--- CreateAGraph/CreateAGraph/GraphView.swift
+++ CreateAGraph/CreateAGraph/GraphView.swift
@@ -8,14 +8,35 @@
 
 import UIKit
 
-class GraphView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
+@IBDesignable class GraphView: UIView {
+    
+    // 1
+    @IBInspectable var startColor: UIColor = .red
+    @IBInspectable var endColor: UIColor = .green
+    
     override func draw(_ rect: CGRect) {
-        // Drawing code
+        
+        // 2
+        let context = UIGraphicsGetCurrentContext()!
+        let colors = [startColor.cgColor, endColor.cgColor]
+        
+        // 3
+        let colorSpace = CGColorSpaceCreateDeviceRGB()
+        
+        // 4
+        let colorLocations: [CGFloat] = [0.0, 1.0]
+        
+        // 5
+        let gradient = CGGradient(colorsSpace: colorSpace,
+                                  colors: colors as CFArray,
+                                  locations: colorLocations)!
+        
+        // 6
+        let startPoint = CGPoint.zero
+        let endPoint = CGPoint(x: 0, y: bounds.height)
+        context.drawLinearGradient(gradient,
+                                   start: startPoint,
+                                   end: endPoint,
+                                   options: [])
     }
-    */
-
 }
