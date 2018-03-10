ee3f1414d6be9f0c31a6c41411e63fb973119e41
saraahmad1114 <asara1114@gmail.com>
2018-03-09 19:35:07 -0500

    defined hourlyTemperature class

--- CreateAGraph/CreateAGraph/HourlyTemperature.swift
+++ CreateAGraph/CreateAGraph/HourlyTemperature.swift
@@ -10,10 +10,16 @@ import Foundation
 
 class HourlyTemperature{
     
+    var time: Double?
+    var temperature: Double?
     
-    
-    
-    
-    
-    
+    init(jsonDictionary: [String: Any]) {
+        guard
+            let time = jsonDictionary["time"] as? Double,
+            let temperature = jsonDictionary["apparentTemperature"] as? Double
+            else {print("did not unwrap contents of jsonDictionary"); return}
+        
+        self.time = time
+        self.temperature = temperature
+    }
 }
