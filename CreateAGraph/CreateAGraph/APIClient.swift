28f9d41ecf25e9f446192f66879daeb98ff4b5d5
saraahmad1114 <asara1114@gmail.com>
2018-03-09 19:18:27 -0500

    done with apiclient

--- CreateAGraph/CreateAGraph/APIClient.swift
+++ CreateAGraph/CreateAGraph/APIClient.swift
@@ -12,15 +12,28 @@ class APIClient{
     
     class func getJSONInformation (completion: @escaping([String: Any])-> ()){
         
-        var jsonDictionary = [String: Any]()
+        var dictionary = [String: Any]()
         
         let url = "https://api.darksky.net/forecast/\(Secrets.apiKey)/40.7380,-73.8801"
         
+        let convertedUrl = URL(string: url)
         
+        guard let unwrappedConvertedUrl = convertedUrl else{print("uwnrappedConvertedUrl did not unwrap"); return}
         
+        let request = URLRequest(url: unwrappedConvertedUrl)
         
-        
-        
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            
+            guard let unwrappedData = data else{print("unwrappedData did not unwrap"); return}
+            
+            let jsonDictionary = try? JSONSerialization.jsonObject(with: unwrappedData, options: []) as! [String: Any]
+            
+            guard let unwrappedJsonDictionary = jsonDictionary else{print("unwrappedJsonDictionary did not unwrap"); return}
+            
+            dictionary = unwrappedJsonDictionary
+            
+            completion(dictionary)
+        }
+        task.resume()
     }
- 
 }
