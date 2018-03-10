e1a3f684729fcd5040563698fc5e35820c2adf98
saraahmad1114 <asara1114@gmail.com>
2018-03-09 20:30:59 -0500

    datastore complete

--- CreateAGraph/CreateAGraph/HourlyTemperatureDatastore.swift
+++ CreateAGraph/CreateAGraph/HourlyTemperatureDatastore.swift
@@ -17,7 +17,24 @@ class HourlyTemperatureDatastore{
     
     func getWeatherHourlyTemperature (completion:@escaping ([HourlyTemperature]) -> ()){
       
-        
+        APIClient.getJSONInformation { (weatherDictionary) in
+            
+            guard let unwrappedWeatherDictionary = weatherDictionary as? [String: Any] else{print("unwrappedWeatherDictionary did not unwrap"); return}
+            
+            guard let hourlyJson = unwrappedWeatherDictionary["hourly"] as? [String: Any] else{print("did not unwrap hourlyJson"); return}
+            
+            guard let dataArray = hourlyJson["data"] as? Array<Any> else{print("dataArray did not unwrap"); return}
+            
+            for singleDictionary in dataArray{
+                
+                guard let unwrappedSingleDictionary = singleDictionary as? [String: Any] else {print("unwrappedSingleDictionary did not unwrap"); return}
+                
+                let hourlyObject = HourlyTemperature.init(jsonDictionary: unwrappedSingleDictionary)
+                
+                self.hourlyTemperatureArray.append(hourlyObject)
+            }
+            
+            completion(self.hourlyTemperatureArray)
+        }
     }
- 
 }
