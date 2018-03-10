8b0534f0b04b3747e810ce62064a2dd6d28f99cc
saraahmad1114 <asara1114@gmail.com>
2018-03-10 11:11:41 -0500

    added class

---
+++ CreateAGraph/CreateAGraph/GraphView.swift
@@ -0,0 +1,21 @@
+//
+//  GraphView.swift
+//  CreateAGraph
+//
+//  Created by Sara Ahmad on 3/10/18.
+//  Copyright © 2018 Sara Ahmad. All rights reserved.
+//
+
+import UIKit
+
+class GraphView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
